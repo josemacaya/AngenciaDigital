dd0dc34a8fed3f1fe10a6e240d4c5eab728c0f98
Luciano <jmacayex@gmail.com>
2026-03-31 23:20:13 -0300

    Commit 3: Mejora en la iteractividad de JavaScript

--- Desktop/AgenciaDigital/CSS/script.js
+++ Desktop/AgenciaDigital/CSS/script.js
@@ -6,4 +6,4 @@ window.onload = function() {
 // Función para cambiar el color del botón al hacer clic
 function saludar() {
     alert("¡Gracias por contactar a Los Tres Ejes Web! Pronto nos comunicaremos contigo.");
-}
+} 
